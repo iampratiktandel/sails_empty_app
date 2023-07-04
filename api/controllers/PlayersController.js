464c5b8e767bfc5194af7c6c0eb35ae7e22dbeb4
PratikTandel <pratik.tandel@1rivet.com>
2023-07-04 19:03:23 +0530

    initial commit

---
+++ api/controllers/PlayersController.js
@@ -0,0 +1,12 @@
+/**
+ * PlayersController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+
+};
+
